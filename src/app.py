e90fa1ec60e248fb221e3d94279e8c842737a569
Jarki <yaroslav.ivashko@gmail.com>
2023-02-05 11:49:24 +0200

    Added logging

--- src/app.py
+++ src/app.py
@@ -1,13 +1,14 @@
 import json
 import os
 import logging
-import time
 
 import requests
 
 import aws_utils
 import utils
 from botocore.exceptions import ClientError
+
+
 
 class Bot:
     def __init__(self, token, api_key) -> None:
@@ -16,58 +17,56 @@ class Bot:
         self.prefix = "https://api.telegram.org/bot"
         self.message_handlers = {}
         
+        self.update_handled = False
+        
     def send_message(self, chat_id, message):
-        return requests.get(f"{self.prefix}{token}/sendMessage", params={
+        responce = requests.get(f"{self.prefix}{token}/sendMessage", params={
             'chat_id': chat_id,
             'text': message,
             'parse_mode':'MarkdownV2'
         })
+         
+        logging.info("Sent a message, responce: %s", responce.text)
+         
+        return responce
         
     def add_command_handler(self, callback, command):
         self.message_handlers[command] = callback
         
     def handle_message(self, chat_id, message):
+        logging.info('Handling message from %s (Message: %s)', message["from"]["username"], message["text"])
+
+        if self.update_handled:
+            logging.info('Update has already been handled')
+            return
+            
         received_text = message["text"]
         
         if received_text in self.message_handlers:
             self.message_handlers[received_text](chat_id, message)
+            
+        self.update_handled = True
+        
 
 token = os.environ.get("TELEGRAM_TOKEN")
-api_key = os.environ.get("SECRET_API_KEY")
+api_key = str(os.environ.get("SECRET_API_KEY"))
 region = os.environ.get("REGION")
 instance_id = os.environ.get("INSTANCE_ID")
 owner_id = int(os.environ.get("OWNER_ID"))
+DEV = int(os.environ.get("DEV"))
 
 # TODO:
 # 1. Log who uses the command to a DynamoDB table, and send it to owner chat
 # 2. Implement allowed users
 # 3. Implement admin users
-# 4. Figure out names in template (change helloWorld stuff) - DONE
-# 5. implement server start-stop, status - DONE
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-    
     if event["headers"]["X-Telegram-Bot-Api-Secret-Token"] != api_key:
+        logging.info("Received an api key that ends with %s", event["headers"]["X-Telegram-Bot-Api-Secret-Token"][-4:])
+        logging.info("True key ends with %s", api_key[-4:])
         return {
             "statusCode": 403,
             "body": "Forbidden"
@@ -87,56 +86,86 @@ def lambda_handler(event, context):
         
         def start_server(chat_id, message):
             try:
-                if aws_utils.get_instance_status(region, instance_id).lower() == 'running':
+                logging.info("Starting server")
+                bot.send_message(chat_id, f"Attempting to start server\\. Please wait")
+                
+                status = aws_utils.get_instance_status(region, instance_id).lower()
+                if status == 'running':
+                    logging.info("Could not start server: it's already running")
+                    
                     public_ip = aws_utils.get_public_ip(region, instance_id)
-                    bot.send_message(owner_id, f"Server is already running\\!\n" + 
+                    bot.send_message(chat_id, f"Server is already running\\!\n" + 
                                     f"Use this IP to connect:\n||{utils.escape_string(public_ip)}||")
                     return
                 
-                bot.send_message(chat_id, f"Starting server")
+                if status == 'pending':
+                    logging.info("Could not start server: it's pending")
+                    
+                    public_ip = aws_utils.get_public_ip(region, instance_id)
+                    bot.send_message(chat_id, f"Server is starting\\!\n" + 
+                                    f"Please wait")
+                    return
+                
+                # Dont start server while in development
+                if DEV == 0:
+                    aws_utils.start_instance(region, instance_id)
                 
                 public_ip = aws_utils.get_public_ip(region, instance_id)
+                if DEV == 1:
+                    public_ip = '192.168.0.1'
+                
                 bot.send_message(chat_id, f"Instance started, starting Valheim dedicated server\nYou will be able to connect shortly\n" +
                                 f"Use this IP to connect:\n||{utils.escape_string(public_ip)}||")
-                aws_utils.start_valheim_server(region, instance_id)
+                
+                logging.info("Server started successfully")
                 
                 # Notify the owner
                 if chat_id != owner_id:
                     bot.send_message(owner_id, f"Server started by @{utils.escape_string(message['from']['username'])}\n")
             except ClientError as e:
-                print(e)
+                logging.error('An error occurred (%s): %s', str(e), e)
                 bot.send_message(chat_id, "An error occured while starting the server")
             except Exception as e:
-                print("Unexpected exception:")
-                print(e)
+                logging.error('An error occurred (%s): %s', str(e), e)
             
         def stop_server(chat_id, message):
             try:
-                if aws_utils.get_instance_status(region, instance_id).lower() == 'stopped':
-                    bot.send_message(owner_id, f"Server is not running\\!\n")
-                    return
+                logging.info("Stopping server")
                 
                 bot.send_message(chat_id, "Stopping server")
+                
+                if aws_utils.get_instance_status(region, instance_id).lower() == 'stopped':
+                    bot.send_message(chat_id, f"Server is not running\\!\n")
+                    return
+                
                 aws_utils.stop_instance(region, instance_id)
                 bot.send_message(chat_id, "Server stopped")
+                logging.info("Server stopped")
                 
                 # Notify the owner
                 if chat_id != owner_id:
                     bot.send_message(owner_id, f"Server stopped by @{utils.escape_string(message['from']['username'])}\n")
             except ClientError as e:
-                print(e)
+                logging.error('An error occurred (%s): %s', str(e), e)
                 bot.send_message(chat_id, "An error occured while stopping the server")
             except Exception as e:
-                print("Unexpected exception:")
-                print(e)
+                logging.error('An error occurred (%s): %s', str(e), e)
             
         def get_server_status(chat_id, message):
             try:
+                logging.info("Retrieving server status")
+                
                 status = aws_utils.get_instance_status(region, instance_id)
                 
                 message = f"The server is {utils.escape_string(status)}\\.\n"   
                         
+                if DEV == 1:
+                    public_ip = '192.168.0.1'
+                    message = message + f"Use ||{utils.escape_string(public_ip)}|| to connect\n"
+                
                 if status == 'running':
+                    logging.info("The server is running, retrieving CPU data")
+                    
                     public_ip = aws_utils.get_public_ip(region, instance_id)
                     message = message + f"Use ||{utils.escape_string(public_ip)}|| to connect\n"
                     
@@ -148,11 +177,10 @@ def lambda_handler(event, context):
                 
                 bot.send_message(chat_id, message)
             except ClientError as e:
-                print(e)
+                logging.error('An error occurred (%s): %s', str(e), e)
                 bot.send_message(chat_id, "An error occured while retrieving server status")
             except Exception as e:
-                print("Unexpected exception:")
-                print(e)
+                logging.error('Unexpected error (%s): %s', str(e), e)
         
         bot.add_command_handler(start_server, "/start_server")
         bot.add_command_handler(stop_server, "/stop_server")
